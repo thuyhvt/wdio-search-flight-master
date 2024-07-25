61123164c60a7521d30a8af816e96687979ccbb3
Hoang Vu Thanh Thuy <thuyhvt@fsoft.com.vn>
2024-07-26 01:36:17 +0700

    test

--- wdio-search-flight-master/test/pageobjects/home.page.ts
+++ wdio-search-flight-master/test/pageobjects/home.page.ts
@@ -5,144 +5,104 @@ import Page from "./page.js";
  * sub page containing specific selectors and methods for a specific page
  */
 class HomePage extends Page {
-  public pickFlightBtn(name: "From" | "To") {
-    return $(`[class*=des__flight_${name.toLowerCase()}]`);
+
+  public btnChooseLocation(name: "From" | "To") {
+    return $(`//input[@name='${name}']`);
   }
 
-  public get flightFromPopup() {
-    return $(".flight_from_popup");
+  public get popupFlightFrom() {
+    return $(".flight_from_destination");
   }
 
-  public get flightToPopup() {
-    return $(".flight_to_popup");
+  public get popupFlightTo() {
+    return $(".flight_to_destination");
   }
 
-  public get inputFromInPopup() {
-    return this.flightFromPopup.$("input[data-id=flight_from]");
+  public get inputFlightFromPopup() {
+    return $(`.flight_from_destination input[data-id='flight_from']`);
   }
 
-  public get inputToInPopup() {
-    return this.flightToPopup.$("input[data-id=flight_to]");
+  public get inputFlightToPopup() {
+    return $(`.flight_to_destination input[data-id='flight_to']`);
   }
 
-  public get destinySuggestionsMenu() {
+  public get listSuggestLocation() {
     return $(".tt-dropdown-menu");
   }
 
-  public get inputFlightPassenger() {
+  public get inputfightPassenger() {
     return $("input#flight_passenger");
   }
 
-  public get passengerPopup() {
+  public get popupFlightPassenger() {
     return $(".pop-flight-passenger");
   }
 
-  public inputDatePicker(type: "Depart" | "Return") {
-    return $(`input[name=${type}]`);
+  public inputDateFlight(type: "Depart" | "Return") {
+    return $(`//input[@name='${type}']`);
   }
 
-  public inputPassengersPopup(type: "adults" | "children" | "infants") {
+  public inputPassengerPopup(type: "adults" | "children" | "infants") {
     return $(`input[data-name='nb_${type}']`);
   }
 
-  public get findFlightsBtn() {
+  public get btnsearchFlight() {
     return $("button#search_button");
   }
 
-  public increasePsgBtn(passengerType: "adults" | "children" | "infants") {
+  public plusBtn(type: "adults" | "children" | "infants") {
     return $(
-      `//*[contains(@class,'pop-flight-passenger')] //*[@class='row' and .//input[@data-name='nb_${passengerType}']] //span[contains(@class,'input-group-btn') and .//*[contains(@class,'btn-plus')]]`
+      `//div[ancestor::*[contains(@class,'pop-flight-passenger')] and @data-field='nb_${type}' and @data-type='plus']//i`
     );
   }
-
-  /**
-   * Get elements of search tabs
-   * @param name: name in Vietnamese
-   * @returns
-   */
-  async getSearchTab(name: "Vé máy bay" | "Khách sạn" | "Tour du lịch") {
-    const searchTabs = {
-      "Vé máy bay": "flight",
-      "Khách sạn": "hotel",
-      "Tour du lịch": "tour",
-    };
-    return await $(`[data-id=${searchTabs[name]}]`);
+  
+  public async homeSearchMenu(tab: string) {
+    return await $(`[data-id=${tab}]`);
   }
 
-  /**
-   * Method select search tab
-   * @param tabName : Name of the tab
-   */
-  public async selectSearchTab(
+  public async selectHomeTab(
     tabName: "Vé máy bay" | "Khách sạn" | "Tour du lịch"
   ): Promise<void> {
-    const tab = await this.getSearchTab(tabName);
-    const tabStatus = await tab.getAttribute("class");
-    if (!tabStatus.includes("active")) {
-      await tab.click();
+    let tab = ""
+    switch(tabName) {
+      case 'Vé máy bay':
+        tab = 'flight'
+        break;
+      case 'Khách sạn':
+        tab = 'hotel'
+        break;
+      default:
+        tab = 'tour'
     }
+    await (await this.homeSearchMenu(tab)).click()
   }
 
-  /**
-   * Method to set flight destiny from., to
-   * @param type: 2 types from, to
-   * @param des: destiny fill in the search
-   */
-  private async selectFlightDestiny(type: "From" | "To", des: string) {
-    const selectFlightBtn = await this.pickFlightBtn(type);
-    const popup =
-      type == "From" ? await this.flightFromPopup : await this.flightToPopup;
-    const input =
-      type == "From" ? await this.inputFromInPopup : await this.inputToInPopup;
-    const isInputDisplayed = await input.isDisplayedInViewport();
-    if (!isInputDisplayed) {
-      await selectFlightBtn.click();
-    }
-    await expect(popup).toBeDisplayed();
+  public async selectLocationFlight(type: "From" | "To", des: string) {
+    let selectFlightBtn = await this.btnChooseLocation(type);
+    await selectFlightBtn.click();
+    let popup =
+      type == "From" ? await this.popupFlightFrom : await this.popupFlightTo;
+    let input =
+      type == "From" ? await this.inputFlightFromPopup : await this.inputFlightToPopup;
+    await expect(popup).toBeDisplayed;
+    await expect(input).toBeDisplayed();
     await input.addValue(des);
-    const option = await $(`strong.tt-highlight.*=${des}`);
-    await expect(option).toBeExisting();
-    await option.click();
+    let listSuggest = await $(`strong.tt-highlight.*=${des}`);
+    await expect(listSuggest).toBeExisting();
+    await listSuggest.click();
   }
 
-  /**
-   * Complete select flight from... to...
-   * @param data
-   */
-  public async selectFlightFromTo(data: { from: string; to: string }) {
-    await this.selectFlightDestiny("From", data.from);
-    await this.selectFlightDestiny("To", data.to);
-  }
-
-  /**
-   * Set date for only once time for Depart flight or Return flight
-   * @param type
-   * @param date
-   */
-  private async setDate(
+  public async setDate(
     type: "Depart" | "Return",
     date: string
   ): Promise<void> {
-    const inputDate = await this.inputDatePicker(type);
-    const inputCss =
-      type == "Depart" ? "#departure_date_flight" : "#returning_date_flight";
+    let inputDate = await this.inputDateFlight(type);
+    let eleInput = type == "Depart" ? "#departure_date_flight" : "#returning_date_flight";
     await browser.execute((ele) => {
-      const input = document.querySelector(ele);
+      let input = document.querySelector(ele);
       input?.removeAttribute("readonly");
-    }, inputCss);
+    }, eleInput);
     await inputDate.setValue(date);
-  }
-
-  /**
-   * Set date for both Depart and Return flight
-   * @param data date should be in format dd/mm/yyyy
-   */
-  public async setDepartReturnDate(data: {
-    depart: string;
-    return: string;
-  }): Promise<void> {
-    await this.setDate("Depart", data.depart);
-    await this.setDate("Return", data.return);
   }
 
   public async setPassengers(passengers?: {
@@ -151,26 +111,22 @@ class HomePage extends Page {
     infants?: number;
   }) {
     if (passengers) {
-      const isPopupPsgDisplayed =
-        await this.passengerPopup.isDisplayedInViewport();
-      if (!isPopupPsgDisplayed) {
-        await this.inputFlightPassenger.click();
-        await expect(this.passengerPopup).toBeDisplayed();
-      }
-      for (const [key, value] of Object.entries(passengers)) {
-        const inputEle = await this.inputPassengersPopup(key as any);
+      await this.inputfightPassenger.click();
+      await expect(this.popupFlightPassenger).toBeDisplayed();
+      for (let [key, value] of Object.entries(passengers)) {
+        let inputEle = await this.inputPassengerPopup(key as any);
         let currentValue = await inputEle.getValue();
-        const gap = value - parseInt(currentValue);
+        let gap = value - parseInt(currentValue);
         for (let i = 0; i < gap; i++) {
-          const increaseBtn = await this.increasePsgBtn(key as any);
+          let increaseBtn = await this.plusBtn(key as any);
           await increaseBtn.waitForClickable();
           await increaseBtn.click();
         }
         currentValue = await inputEle.getValue();
         expect(parseInt(currentValue)).toEqual(value);
       }
-    }
   }
+ }
 }
 
 export default new HomePage();
--- wdio-search-flight-master/test/specs/test.e2e.ts
+++ wdio-search-flight-master/test/specs/test.e2e.ts
@@ -2,28 +2,21 @@ import { expect } from "@wdio/globals";
 import homePage from "../pageobjects/home.page.js";
 import flightPage from "../pageobjects/flight.page.js";
 
-describe("Search for flights", () => {
-  it("should return results for flights", async () => {
+describe("Search Flights", () => {
+  it("it should be return results match with flights", async () => {
     await homePage.open();
-    await homePage.pickFlightBtn("From").waitForExist();
-    await homePage.selectSearchTab("Vé máy bay");
-    await expect(homePage.getSearchTab("Vé máy bay")).toHaveAttribute(
-      "class",
-      /active/
-    );
-    await homePage.selectFlightFromTo({
-      from: "Nội Bài",
-      to: "Tân Sơn Nhất",
-    });
-    await homePage.setDepartReturnDate({
-      depart: "30/07/2024",
-      return: "15/08/2024",
-    });
+    await browser.maximizeWindow();
+    await homePage.btnsearchFlight.waitForDisplayed();
+    await homePage.selectHomeTab("Vé máy bay");
+    await homePage.selectLocationFlight("From", "Nội Bài" );
+    await homePage.selectLocationFlight("To", "Tân Sơn Nhất" );
+    await homePage.setDate("Depart", "30/07/2024");
+    await homePage.setDate("Return", "15/08/2024");
     await homePage.setPassengers({
       adults: 2,
       children: 1,
     });
-    await homePage.findFlightsBtn.click();
+    await homePage.btnsearchFlight.click();
     await expect(driver).toHaveUrl(/tim-kiem-ve/);
     await flightPage.verifyFlightsLoaded()
     await expect(flightPage.flightDepartList).toBeDisplayed();
